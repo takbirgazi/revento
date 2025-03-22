78be6f6189ca58c56a8ba4efa9116ffe6ff2245c
takbirgazi <takbirgazibd@gmail.com>
2025-03-22 18:36:56 +0600

    Fixed mobile menu and add Service

---
+++ src/app/service/page.tsx
@@ -0,0 +1,66 @@
+import Faq from "@/components/HomeComp/Faq/Faq";
+import Highlights2 from "@/components/HomeComp/Highlights2/Highlights2";
+import Testimonials from "@/components/HomeComp/Testimonials/Testimonials";
+import Services from "@/components/ServicesCom/Services/Services";
+import HeroCustom from "@/components/ShareComp/HeroCustom/HeroCustom";
+import Image from "next/image";
+
+const page = () => {
+    const heroData = {
+        topHeader: "Services",
+        mainHeader: "our services",
+        desc: "Experience growth through innovative digital marketing designed to deliver results.",
+        topHeaderColor: "#1cb785",
+        mainHeaderColor: "white",
+    }
+
+    return (
+        <div>
+            <HeroCustom heroData={heroData} />
+            <div className="container mx-auto px-5">
+                <div className="py-12 flex flex-col lg:flex-row justify-baseline items-center gap-5">
+                    <div className='w-full lg:w-1/2'>
+                        <h2 className='py-10 text-2xl lg:text-7xl font-medium uppercase font-anton'>
+                            <span className=" text-[#051a2f] tracking-tight">affordable pricing plans for </span>
+                            <span className=" text-[#828d97] tracking-tight"> all your digital marketing services</span>
+                        </h2>
+                        <p className="text-gray-500">At Revento, we are more than a digital marketing agency—we are your growth partners. Founded with a mission to transform ideas into impactful results, we specialize in delivering innovative strategies that drive measurable success.</p>
+                    </div>
+                    <div data-aos="fade-up" className='w-full lg:w-1/2 relative'>
+                        <figure>
+                            <Image className='w-full h-full' src="https://framerusercontent.com/images/kef0yFNKhK1mGIbvNgjaSMq7yOA.png?scale-down-to=1024" height={550} width={450} alt='Feature' />
+                        </figure>
+                        <figure className="absolute top-0 left-[15%]">
+                            <Image className=" w-[70px]" src="https://framerusercontent.com/images/CKLTdNjhz9KoDE3bA5jdxeiGa4o.png" alt="Icon" height={200} width={200} />
+                        </figure>
+                        <figure className="absolute bottom-0 right-[15%]">
+                            <Image className=" w-[70px]" src="https://framerusercontent.com/images/VJfhCUd5WwmzhreVEXOcwKQ8Zk8.png" alt="Icon" height={200} width={200} />
+                        </figure>
+                    </div>
+                </div>
+                <Services />
+                <Testimonials />
+                <div className='max-w-4xl mx-auto py-16'>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+                        <figure>
+                            <Image src="https://framerusercontent.com/images/dCeA12BmBXiH1uY21XEDNyvahEo.png" height={30} width={200} alt="Nothing" />
+                        </figure>
+                        <figure>
+                            <Image src="https://framerusercontent.com/images/bPzdBnzcj0xZ9xquFthgnDKXo.png" height={30} width={200} alt="Nothing" />
+                        </figure>
+                        <figure>
+                            <Image src="https://framerusercontent.com/images/yaNXlI28UkUPncBvBZP6Q98EGuY.png" height={30} width={200} alt="Nothing" />
+                        </figure>
+                        <figure>
+                            <Image src="https://framerusercontent.com/images/dvqYX9ork1jB4TUSvDZN5pM1YU.png" height={30} width={200} alt="Nothing" />
+                        </figure>
+                    </div>
+                </div>
+                <Faq />
+                <Highlights2 />
+            </div>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/components/ServicesCom/Services/Services.tsx
@@ -0,0 +1,82 @@
+import CardMd from '@/components/ShareComp/CardMd/CardMd';
+import SectionHeader from '@/components/ShareComp/SectionHeader/SectionHeader';
+
+const Services = () => {
+    const headerData = {
+        topHeading: "Our Services!",
+        topHeadingColor: "#1cb785",
+        heading: "we provide quality services",
+        headingColor: "#051a2f"
+    }
+    const cardData = [
+        {
+            id: 1,
+            title: "Search Engine Optimization",
+            desc: "Boost your website’s visibility and attract organic traffic with SEO tailored to your business goals.",
+            link: "/",
+            bgColor: "#ff8f27",
+            topImageLink: "https://framerusercontent.com/images/HaTeCnedqptj5oZx1ZHkM2pcbZg.svg",
+            bottomImageLink: "https://framerusercontent.com/images/TK6PAMZN32ze4HffAHGSjINGCE.svg"
+        },
+        {
+            id: 2,
+            title: "Content Marketing",
+            desc: "Enhance your brand’s online presence and drive engagement with a strategy customized to your needs.",
+            link: "/",
+            bgColor: "#9d89fc",
+            topImageLink: "https://framerusercontent.com/images/mP6yDpIu2HzOfRG2Qlr2z6O28O0.svg",
+            bottomImageLink: "https://framerusercontent.com/images/idbDbcyPRv2S59MVMgWh9L7LA.svg"
+        },
+        {
+            id: 3,
+            title: "Social Media Marketing",
+            desc: "Grow your audience and increase conversions with targeted digital marketing solutions.",
+            link: "/",
+            bgColor: "#ff4f3f",
+            topImageLink: "https://framerusercontent.com/images/7os4g4ruRwOonodXRETIWnxw1M.svg",
+            bottomImageLink: "https://framerusercontent.com/images/hGSozs8NwWZ077Sm2JLNNdlBwQw.svg"
+        },
+        {
+            id: 4,
+            title: "Email Marketing",
+            desc: "Maximize your online reach and generate leads with content tailored to your audience.",
+            link: "/",
+            bgColor: "#ff4f3f",
+            topImageLink: "https://framerusercontent.com/images/7os4g4ruRwOonodXRETIWnxw1M.svg",
+            bottomImageLink: "https://framerusercontent.com/images/hGSozs8NwWZ077Sm2JLNNdlBwQw.svg"
+        },
+        {
+            id: 5,
+            title: "Influencer Marketing",
+            desc: "Elevate your business with a social media strategy designed for your growth objectives.",
+            link: "/",
+            bgColor: "#1cb785",
+            topImageLink: "https://framerusercontent.com/images/HaTeCnedqptj5oZx1ZHkM2pcbZg.svg",
+            bottomImageLink: "https://framerusercontent.com/images/idbDbcyPRv2S59MVMgWh9L7LA.svg"
+        },
+        {
+            id: 6,
+            title: "Analytics and Reporting",
+            desc: "Expand your customer base and improve visibility with data-driven advertising campaigns.",
+            link: "/",
+            bgColor: "#051a2f",
+            topImageLink: "https://framerusercontent.com/images/7os4g4ruRwOonodXRETIWnxw1M.svg",
+            bottomImageLink: "https://framerusercontent.com/images/hGSozs8NwWZ077Sm2JLNNdlBwQw.svg"
+        },
+    ]
+    return (
+        <div className='py-10'>
+            <div data-aos="fade-up" className='flex flex-col lg:flex-row justify-between lg:items-end gap-5'>
+                <SectionHeader headerData={headerData} />
+                <p className='w-full lg:w-1/2 text-center lg:text-left'>Experience growth through innovative digital marketing designed to reach, inspire, and deliver results.</p>
+            </div>
+            <div className='py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10'>
+                {
+                    cardData.map(card => <CardMd key={card.id} cardData={card} />)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
